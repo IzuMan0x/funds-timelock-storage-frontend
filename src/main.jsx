9e10c8b4b924778e10e860f7d28cb8d8551f757d
IzuMan0x <mcasterchief@gmail.com>
2023-08-04 01:50:07 +0900

    fix background style
    🚀

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div
-      className="bg-[url('./assets/finance-background.jpg')] bg-cover h-screen w-screen flex justify-center
+      className="bg-[url('./assets/finance-background.jpg')] bg-cover h-screen w-screen flex justify-center bg-center md:bg-contain
     "
     >
       <App />
